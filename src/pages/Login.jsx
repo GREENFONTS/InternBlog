acc6e943a8eb1409deabc2df6d42f8e28ea82da7
GREENFONTS <godwillonyewuchii@gmail.com>
2022-11-27 19:38:08 +0100

    fix : chnanged text

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login({ setIsAuth }) {
     <div className="flex justify-center items-center h-screen w-full">
       <div className="flex flex-col gap-y-9 justify-center p-3 items-center shadow w-1/9 sm:w-1/2 xl:w-1/3 mx-auto h-[400px]">
         <div className="flex flex-col gap-y-6 justify-center items-center">
-          <h2 className="font-sans text-2xl md:text-3xl font-semibold">Welcome back 😊</h2>
+          <h2 className="font-sans text-2xl md:text-3xl font-semibold">Are you a Blogger? 😊</h2>
           <p className="font-sans text-md md:text-xl font-medium">Continue with Google</p>
         </div>
         <button className="shadow p-3 md:p-5 m-3 rounded-lg bg-blue-400" onClick={signInWithGoogle}>
